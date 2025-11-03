28f63c2ac71e2eb9969daed15bb3b23576f3067e
ris2002 <72793209+ris2002@users.noreply.github.com>
2025-11-03 15:53:38 +0000

    Modified ingest_data_file

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,11 +2,6 @@ import kagglehub
 from kagglehub import KaggleDatasetAdapter
 from pipelines.training_pipeline import training_pipeline
 output_csv_path = "sentiment140.csv"
-file_path = ""
-data_file = kagglehub.load_dataset(
-    KaggleDatasetAdapter.PANDAS,
-    "kazanova/sentiment140",  
-    file_path,             
-)
+data_file=""
 if __name__ =="__main__":
     training_pipeline(data_file)
--- steps/ingest_data.py
+++ steps/ingest_data.py
@@ -1,6 +1,27 @@
 import pandas as pd
 from zenml import step
 import logging
-@step
-def ingest_data(data_path:pd.DataFrame)->None:
-    pass
+
+
+class IngestData:
+    def __init__(self, data_path: str):
+        self.data_path = data_path
+
+    def getData(self):
+        df = pd.read_csv(self.data_path)
+        print('ingest dATA')
+        return df
+
+
+@step()
+def ingest_data(data_path: str) -> pd.DataFrame:
+
+    try:
+        loader = IngestData(data_path)
+        df = loader.getData()
+        
+        return df
+
+    except Exception as e:
+        logging.error(f"Error while ingesting data: {e}")
+        raise e
